73a16976cd92f6414640bf37f13d94a5373243ae
Wei <5h3ll3x@gmail.com>
2020-01-27 11:17:35 +0900

    feat: find the success record if user has passed kyc

--- controllers/account.js
+++ controllers/account.js
@@ -65,11 +65,20 @@ module.exports = {
       where: { id: user.id },
       attributes: model.UserAttrs
     });
-    const latestKycRecord = await model.Kyc.findOne({
-      where: { userId: existed.id },
-      attributes: ["resultCode", "resultMessage", "errorMessage", "realName", "realId", "uniqueHash", "passedAt"],
-      order: [["createdAt", "DESC"]]
-    });
+    let latestKycRecord = null
+    if (existed.kycState === 1) {
+      latestKycRecord = await model.Kyc.findOne({
+        where: { resultMessage: 'SUCCESS', userId: existed.id },
+        attributes: ["resultCode", "resultMessage", "errorMessage", "realName", "realId", "uniqueHash", "passedAt"],
+        order: [["createdAt", "DESC"]]
+      });
+    } else {
+      latestKycRecord = await model.Kyc.findOne({
+        where: { userId: existed.id },
+        attributes: ["resultCode", "resultMessage", "errorMessage", "realName", "realId", "uniqueHash", "passedAt"],
+        order: [["createdAt", "DESC"]]
+      });
+    }
     existed = existed.get({ plain: true })
     existed.kyc = latestKycRecord
     ctx.body = { status: 'success', data: existed };
