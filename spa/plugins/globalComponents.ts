d71b3ed447f2b1afeca3600c67b73ae0a3cf2146
Wei <5h3ll3x@gmail.com>
2020-01-27 10:50:43 +0900

    feat: allow copying address and contacts; auto link tel numbers; color the model

--- spa/plugins/globalComponents.ts
+++ spa/plugins/globalComponents.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Loading from '@/components/basic/Loading.vue'
+import VueClipboard from 'vue-clipboard2'
 
 import 'animate.css'
+
+VueClipboard.config.autoSetContainer = true
+Vue.use(VueClipboard)
 
 Vue.component('loading', Loading)
